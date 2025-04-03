8ece077d198cbc6f6df06ddb0cac0556ebdbf816
matveevaolga <matveevaolga2882@gmail.com>
2025-04-03 19:25:23 +0300

    init commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Olga Matveeva on 03.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
